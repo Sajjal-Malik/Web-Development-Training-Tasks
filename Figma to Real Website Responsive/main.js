716d244bdad8de6eea9791c44e99d37952e9b61c
SajjalMalik <sajjalmalik.sm@gmail.com>
2025-03-19 22:39:40 +0500

    Modified CSS file in Figma Project

--- Figma to Real Website Responsive/main.js
+++ Figma to Real Website Responsive/main.js
@@ -1,10 +1,13 @@
  const nav = document.querySelector("nav");
+ const movileNav = document.querySelector("mobile-nav");
 
  window.addEventListener("scroll", () => {
     if(window.pageYOffset > 60){
         nav.classList.add("scrolled");
+        movileNav.classList.add("scrolled");
     }
     else{
         nav.classList.remove("scrolled");
+        movileNav.classList.remove("scrolled");
     }
  })
